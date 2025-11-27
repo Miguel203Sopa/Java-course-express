8070c557a5498ea3340849ff0765f9a9a625ecde
Miguel203Sopa <migue.guevara989@gmail.com>
2025-11-27 15:22:45 -0600

    resolucion de primeras 3 guias

--- SQL-Queries/PracticaMDB_2C.sql
+++ SQL-Queries/PracticaMDB_2C.sql
@@ -2,23 +2,101 @@ CREATE DATABASE HotelDB
 USE HotelDB
 
 CREATE TABLE Hotel(
-	CodigoHotel char(6),
-	Nombre varchar(30),
+	CodigoHotel char(6) NOT NULL,
+	Nombre varchar(30) UNIQUE,
 	Direccion varchar(60),
-	SitioWeb varchar(60)
+	SitioWeb varchar(60) UNIQUE
 );
 
 CREATE TABLE Habitacion(
-	CodigoHabitacion char(6),
+	CodigoHabitacion char(6) NOT NULL,
 	CodigoHotel char(6),
 	Tipo varchar(30),
-	Precio double
-);
+	Precio float
+	);
 
 CREATE TABLE Huesped (
-CodigoHuesped char(6),
+CodigoHuesped char(6)NOT NULL,
 Nombre varchar(30),
-Email varchar (30),
+Email varchar (30) UNIQUE,
 Direccion varchar(50),
 Telefono varchar(8)
 );
+
+CREATE TABLE Reservacion (
+	CodigoReserva char(6) NOT NULL,
+	CodigoHotel char(6) NOT NULL,
+	CodigoHuesped char(6) NOT NULL,
+	CodigoHabitacion char(6) NOT NULL,
+	FechaInicio DATE ,
+	FechaFin DATE
+);
+
+
+ ALTER TABLE Habitacion
+ ADD CONSTRAINT price_check
+ CHECK (Precio> 25); /*Añade a una restriccion*/
+
+  ALTER TABLE Habitacion
+ ADD CONSTRAINT type_check
+ CHECK (Tipo LIKE 'Doble' or Tipo LIKE'doble' or Tipo LIKE 'Individual' or Tipo LIKE 'individiual'); /*Añade a una restriccion*/
+ 
+
+  ALTER TABLE Reservacion
+ ADD CONSTRAINT date_rec_check
+ CHECK(FechaFin > FechaInicio);
+
+
+  ALTER TABLE Reservacion
+ ADD CONSTRAINT date_d_check
+DEFAULT getdate() FOR FechaInicio ;
+
+--Llaves primarias
+
+ALTER TABLE Hotel 
+ADD CONSTRAINT PK_CodigoHotel
+PRIMARY KEY(CodigoHotel)
+GO
+
+ALTER TABLE Habitacion
+ADD CONSTRAINT PK_CodigoHabitacion
+PRIMARY KEY(CodigoHabitacion)
+GO
+
+ALTER TABLE Huesped
+ADD CONSTRAINT PK_CodigoHuesped
+PRIMARY KEY(CodigoHuesped)
+GO
+
+ALTER TABLE Reservacion
+ADD CONSTRAINT PK_CodigoReserva
+PRIMARY KEY(CodigoReserva)
+GO
+
+
+--llaves Foraneas 
+
+ALTER TABLE Reservacion
+ADD CONSTRAINT FK_CodigoHotel
+FOREIGN KEY(CodigoHotel)
+REFERENCES Hotel(CodigoHotel)
+GO
+
+ALTER TABLE Reservacion
+ADD CONSTRAINT FK_CodigoHuesped
+FOREIGN KEY(CodigoHuesped)
+REFERENCES Huesped(CodigoHuesped)
+GO
+
+ALTER TABLE Reservacion
+ADD CONSTRAINT FK_CodigoHabitacion
+FOREIGN KEY(CodigoHabitacion)
+REFERENCES Habitacion(CodigoHabitacion)
+GO
+
+
+
+
+
+
+
