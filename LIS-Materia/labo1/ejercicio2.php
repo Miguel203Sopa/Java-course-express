4bcc9cf5dc6998ee33f87b17fe0d94a500cb3abc
Miguel Vega <150635001+Miguel203Sopa@users.noreply.github.com>
2026-02-04 21:27:29 -0600

    Delete LIS-Materia directory

--- LIS-Materia/labo1/ejercicio2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("PI",3.1416);
-    const PAIS = "El Salvador";
-    echo "El valor de PI es: ".PI."\n";
-    echo "Pais: ".PAIS."\n";
-
-?>
--- LIS-Materia/labo1/ejercicio3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $contador =0;
-    $contador++;
-    echo "Contador: ".$contador."\n";
-    $contador +=5;
-    echo "Contador".$contador."\n";
-    $contador--;
-    echo "Contador: ".$contador."\n";
-?>
