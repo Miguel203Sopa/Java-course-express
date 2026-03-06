bcdc8d98ea1f7edc3c31df16432e4a0ab968b9f9
Miguel203Sopa <migue.guevara989@gmail.com>
2026-03-06 15:22:39 -0600

    Version final del crud template

---
+++ template-space/template-v5/crudTemplateIComplete/src/main/java/data/service/ParentService.java
@@ -0,0 +1,44 @@
+package data.service;
+
+import data.domain.Parent;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.Date;
+import java.util.List;
+
+@Stateless
+public class ParentService {
+
+    @PersistenceContext(unitName = "templatePU")
+    private EntityManager em;
+
+    public void create(Parent parent) {
+
+        parent.setCreatedAt(new Date());
+        em.persist(parent);
+
+    }
+
+    public List<Parent> findAll() {
+        return em.createQuery("SELECT p FROM Parent p", Parent.class)
+                .getResultList();
+    }
+
+    public void update(Parent parent){
+        em.merge(parent);
+    }
+
+    public Parent find(Long id) {
+        return em.find(Parent.class, id);
+    }
+
+    public void delete(Long id) {
+        Parent p = em.find(Parent.class, id);
+        if (p != null) em.remove(p);
+    }
+
+
+
+}
